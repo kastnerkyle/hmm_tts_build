1e7a203d14322ec1ff950c417dc9ac1fb401e9ed
Kyle Kastner <kastnerkyle@gmail.com>
2019-02-23 07:01:16 +0000

    fixed scripts a bit

--- install_voices.sh
+++ install_voices.sh
@@ -1,7 +1,8 @@
 # https://ubuntuforums.org/archive/index.php/t-751169.html
 # https://ubuntuforums.org/showthread.php?t=2271517
 pushd .
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPT=`readlink -f ${BASH_SOURCE[0]} || greadlink -f ${BASH_SOURCE[0]}`
+SCRIPTPATH=`dirname "$SCRIPT"`
 mkdir -p $SCRIPTPATH/hts
 cp -pr $SCRIPTPATH/*.tar.gz $SCRIPTPATH/hts
 cd $SCRIPTPATH/hts
@@ -19,7 +20,7 @@ cd ..
 cd flite+hts_engine-1.06/
 
 ./configure \
-     --with-hts-engine-header-path="$HOME"/hts/hts_engine_API-1.09/include \
-     --with-hts-engine-library-path="$HOME"/hts/hts_engine_API-1.09/lib
+     --with-hts-engine-header-path="$SCRIPTPATH"/hts/hts_engine_API-1.09/include \
+     --with-hts-engine-library-path="$SCRIPTPATH"/hts/hts_engine_API-1.09/lib
 make
 popd
--- say_it.sh
+++ say_it.sh
@@ -1,4 +1,10 @@
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+if [ $# -ne 2 ]; then
+    echo "say_it.sh <text to say in quptes> <output_file_wav>"
+    echo 'Example: ./say_it.sh "hello world" output.wav'
+    exit 1
+fi
+SCRIPT=`readlink -f ${BASH_SOURCE[0]} || greadlink -f ${BASH_SOURCE[0]}`
+SCRIPTPATH=`dirname "$SCRIPT"`
 echo $1 > $SCRIPTPATH/tmp.txt
-$SCRIPTPATH/hts/flite+hts_engine-1.06/bin/flite_hts_engine -m $SCRIPTPATH/hts/hts_voice_cmu_us_arctic_slt-1.05/cmu_us_arctic_slt.htsvoice -s 44100 -o output.wav $SCRIPTPATH/tmp.txt
+$SCRIPTPATH/hts/flite+hts_engine-1.06/bin/flite_hts_engine -m $SCRIPTPATH/hts/hts_voice_cmu_us_arctic_slt-1.05/cmu_us_arctic_slt.htsvoice -s 44100 -o $2 $SCRIPTPATH/tmp.txt
 rm $SCRIPTPATH/tmp.txt
